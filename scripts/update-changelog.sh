c9f6b0f821a3329e9631337b0b1fd339bf784e04
DJ Mountney <dj@currents.dev>
2025-06-10 12:09:12 -0700

    chore: release/currents 0.4.0 (#34)
    * chore: Update version to 0.4.0
    
    * chore: update-changelog-script

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v $PWD:/tmp -w /tmp gembaadvantage/uplift changelog --no-stage 
+docker run --rm -v $PWD:/opt/currents -e "GIT_CONFIG_PARAMETERS='safe.directory'='/opt/currents'" -w /opt/currents gembaadvantage/uplift changelog --no-stage 
