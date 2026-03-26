d90063de264e0f40a6e317682bb9636489b82530
DJ Mountney <dj@currents.dev>
2026-03-26 09:56:48 -0700

    feat: January Currents Release images

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --rm -v $PWD:/opt/currents -e "GIT_CONFIG_PARAMETERS='safe.directory'='/opt/currents'" -w /opt/currents gembaadvantage/uplift changelog --no-stage 
+VERSION=$(grep '^version:' charts/currents/Chart.yaml | awk '{print $2}')
+
+docker run --rm -v "$PWD:/app" \
+  -e "GIT_CONFIG_PARAMETERS='safe.directory'='/app'" \
+  -w /app ghcr.io/orhun/git-cliff/git-cliff --tag "currents-${VERSION}" -u --prepend CHANGELOG.md
